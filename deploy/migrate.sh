2c71d697acca28d001ca1cbe1a17de3b43c09bda
Eric <waterfall.strong@gmail.com>
2022-12-01 21:44:49 -0700

    added deploy scripts

---
+++ deploy/migrate.sh
@@ -0,0 +1,5 @@
+joed tx wasm migrate joe18yn206ypuxay79gjqv6msvd9t2y49w4fz8q7fyenx5aggj0ua37qnv0qf3 28 \
+  --node https://joe-rpc.polkachu.com:443 \
+  --chain-id joe-1 --from steak-admin \
+  --gas-prices 1.1ujoe --gas 5000000 \
+  --gas-adjustment 1.5 -y -b block
---
+++ deploy/upload.sh
@@ -0,0 +1,5 @@
+joed tx wasm store ./artifacts/pfc_steak_hub.wasm \
+  --node https://joe-rpc.polkachu.com:443 \
+  --chain-id joe-1 --from steak-admin \
+  --gas-prices 1.1ujoe --gas 5000000 \
+  --gas-adjustment 1.5 -y -b block
