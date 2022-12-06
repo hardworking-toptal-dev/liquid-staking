dc169f3f99f8ca3488a3e1aa07c9481d5b04f10b
Eric <waterfall.strong@gmail.com>
2022-12-06 14:30:11 -0700

    added add_validator script

---
+++ deploy/add_validator.sh
@@ -0,0 +1,6 @@
+joed tx wasm execute joe18yn206ypuxay79gjqv6msvd9t2y49w4fz8q7fyenx5aggj0ua37qnv0qf3 \
+  '{ "add_validator": { "validator":"joevaloper1gw3yrwgehpslyhlrqjcfyfm24a520z7cj9r8ev" } }' \
+  --node https://joe-rpc.polkachu.com:443 \
+  --chain-id joe-1 --from steak-admin \
+  --gas-prices 1.1ujoe --gas 5000000 \
+  --gas-adjustment 1.5 -y -b block 
